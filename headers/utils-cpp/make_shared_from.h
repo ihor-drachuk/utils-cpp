7c8148c4dc6532bb54c0d6a2b84a41adba886203
Ihor Drachuk <ihor.drachuk@gmail.com>
2021-05-27 18:25:08 +0300

    Added make_shared_from

---
+++ headers/utils-cpp/make_shared_from.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <memory>
+
+template<typename T>
+std::shared_ptr<T> make_shared_from(const T& rhs) { return std::make_shared<T>(rhs); }
+
+template<typename T>
+std::unique_ptr<T> make_unique_from(const T& rhs) { return std::make_unique<T>(rhs); }
