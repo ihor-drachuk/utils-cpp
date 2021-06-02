7d742e21033a19234e1c7763021b2f3555982459
Ihor Drachuk <ihor.drachuk@gmail.com>
2021-06-02 20:54:12 +0300

    scoped_guard: added 'reset' method

--- headers/utils-cpp/scoped_guard.h
+++ headers/utils-cpp/scoped_guard.h
@@ -6,21 +6,25 @@ public:
     scoped_guard(Functor f): m_f(f) {}
     scoped_guard(const scoped_guard&) = delete;
     scoped_guard(scoped_guard&& rhs): m_f(rhs.m_f) { rhs.m_moved = true; }
-    ~scoped_guard() { if (!m_moved) m_f(); }
+    ~scoped_guard() { if (!m_moved && !m_reset) m_f(); }
 
     scoped_guard<Functor>& operator=(const scoped_guard<Functor>&) = delete;
     scoped_guard<Functor>& operator=(scoped_guard<Functor>&& rhs) {
         if (this == &rhs) return *this;
 
         m_f = rhs.m_f;
+        m_reset = rhs.m_reset;
         rhs.m_moved = true;
 
         return *this;
     }
 
+    void reset() { m_reset = true; }
+
 private:
     Functor m_f;
     bool m_moved { false };
+    bool m_reset { false };
 };
 
 template<typename Functor>
