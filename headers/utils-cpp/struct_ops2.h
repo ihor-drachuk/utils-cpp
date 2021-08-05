7ed3363cb9318e8c7214338657c1bb90066af628
Ihor Drachuk <ihor.drachuk@gmail.com>
2021-08-06 01:03:43 +0300

    Improve struct_ops2.h

--- headers/utils-cpp/struct_ops2.h
+++ headers/utils-cpp/struct_ops2.h
@@ -1,28 +1,64 @@
 #pragma once
-#include "tuple_for_each.h"
-#include <cstring>
-#include <cmath>
 #include <tuple>
+#include <cmath>
+#include <numeric>
+
+/*
+  Fast way to add all comparison operators for custom struct.
+
+  All floating-point numbers already handled.
+
+  Global comparison operators are re-used for all types in struct,
+  but any of them could be overriden for your custom struct context.
+
+
+    Example
+  -----------
+
+  struct My_Struct
+  {
+      int a_;
+      double b_;
+      char* s_;
+
+      auto tie() const { return std::tie(a_, b_, s_); }
+  };
+
+
+  // Custom comparer example:  strcmp instead of raw pointers comparison.
+  //                           Notice. It's non-global, but only for struct_ops2.
+  inline int compare(const My_Struct&, const char* a, const char* b)
+  {
+      return strcmp(a, b);
+  }
+
+
+  TIED_COMPARISONS2(My_Struct)
+*/
+
 
 namespace struct_ops_internal {
 
-template<typename T>
+template<typename Struct, typename T>
 typename std::enable_if<!std::is_floating_point<T>::value, int>::type
-inline compare(T a, T b)
+inline compare(const Struct&, T a, T b)
 {
     return (a > b) ? 1 :
            (a < b) ? -1 :
                      0;
 }
 
-inline int compare(const char* a, const char* b)
+/*
+template<typename Struct>
+inline int compare(const Struct&, const char* a, const char* b)
 {
-    return strcmp(a, b);
+    return (a == b) ? 0 : strcmp(a, b);
 }
+*/
 
-template <typename T>
+template <typename Struct, typename T>
 typename std::enable_if<std::is_floating_point<T>::value, int>::type
-inline compare(T a, T b)
+inline compare(const Struct&, T a, T b)
 {
     if (std::fabs(a - b) <= std::numeric_limits<T>::epsilon()) {
         return 0;
@@ -31,15 +67,27 @@ inline compare(T a, T b)
     }
 }
 
-template<typename StructTuple>
-inline int compareTuples(const StructTuple& lhs, const StructTuple& rhs)
+template<typename Struct, std::size_t I = 0, typename... Tp>
+inline typename std::enable_if<I == sizeof...(Tp), int>::type
+compare(const Struct&, const std::tuple<Tp...>&, const std::tuple<Tp...>&)
 {
-    auto result = for_each_pair_par<int>(lhs, rhs, [](int, const auto& v1, const auto& v2, const std::optional<int>&) -> std::optional<int> {
-        const auto result = compare(v1, v2);
-        return (result == 0) ? std::nullopt : std::optional<int>(result);
-    }, std::nullopt, true);
+    return 0;
+}
 
-    return result.value_or(0);
+template<typename Struct, std::size_t I = 0, typename... Tp>
+inline typename std::enable_if<I < sizeof...(Tp), int>::type
+compare(const Struct& s, const std::tuple<Tp...>& t1, const std::tuple<Tp...>& t2)
+{
+    auto result = compare(s, std::get<I>(t1), std::get<I>(t2));
+
+    return result ? result :
+                    compare<Struct, I + 1, Tp...>(s, t1, t2);
+}
+
+template<typename Struct, typename... Tp>
+inline int compareTuples2(const Struct& s, const std::tuple<Tp...>& lhs, const std::tuple<Tp...>& rhs)
+{
+    return compare(s, lhs, rhs);
 }
 
 } // namespace struct_ops_internal
@@ -47,7 +95,7 @@ inline int compareTuples(const StructTuple& lhs, const StructTuple& rhs)
 #define TIED_OP2(STRUCT, OP) \
     inline bool operator OP(const STRUCT& lhs, const STRUCT& rhs) \
     { \
-        const auto result = struct_ops_internal::compareTuples(lhs.tie(), rhs.tie()); \
+        const auto result = struct_ops_internal::compareTuples2(lhs, lhs.tie(), rhs.tie()); \
         return (result OP 0); \
     }
 
@@ -58,15 +106,3 @@ inline int compareTuples(const StructTuple& lhs, const StructTuple& rhs)
     TIED_OP2(STRUCT, <=) \
     TIED_OP2(STRUCT, >) \
     TIED_OP2(STRUCT, >=)
-
-//    Example
-//  -----------
-//
-//  struct My_Struct
-//  {
-//      int a_;
-//      double b_;
-//      auto tie() const { return std::tie(a_, b_); }
-//  };
-//
-//  TIED_COMPARISONS2(My_Struct)
---
+++ tests/test/test3_struct_ops2.cpp
@@ -0,0 +1,119 @@
+#include <gtest/gtest.h>
+#include <utils-cpp/struct_ops2.h>
+
+namespace {
+
+char charArray1[] = {'S', 't', 'r', 0, '1'};
+char charArray2[] = {'S', 't', 'r', 0, '2'};
+const int* const intPtr1 = (const int*)charArray1;
+const int* const intPtr2 = (const int*)charArray2;
+
+enum class ComparisonTest
+{
+    EQ,
+    NE,
+    LT,
+    LE,
+    GT,
+    GE
+};
+
+struct MyStruct
+{
+    int a;
+    float b;
+    char* c;
+    const int* d;
+
+    auto tie() const { return std::tie(a, b, c, d); }
+};
+
+int compare(const MyStruct&, const int* lhs, const int* rhs)
+{
+    if (lhs == rhs) return 0;
+    return strcmp((const char*)lhs, (const char*)rhs);
+}
+
+TIED_COMPARISONS2(MyStruct);
+
+
+class StructOps3TestSuite : public testing::TestWithParam<std::tuple<MyStruct, MyStruct, ComparisonTest, bool>>
+{
+};
+
+void test(const MyStruct& s1, const MyStruct& s2, ComparisonTest comparison, bool result)
+{
+    // Ensure these pointers are different, and data different as well. But strcmp == 0.
+    // Otherwise test isn't valid.
+    ASSERT_NE(intPtr1, intPtr2);
+    ASSERT_NE(memcmp(charArray1, charArray2, sizeof(charArray1)), 0);
+    ASSERT_EQ(strcmp(charArray1, charArray2), 0);
+
+    bool answer = false;
+
+    switch (comparison) {
+        case ComparisonTest::EQ: answer = (s1 == s2); break;
+        case ComparisonTest::NE: answer = (s1 != s2); break;
+        case ComparisonTest::LT: answer = (s1  < s2); break;
+        case ComparisonTest::LE: answer = (s1 <= s2); break;
+        case ComparisonTest::GT: answer = (s1  > s2); break;
+        case ComparisonTest::GE: answer = (s1 >= s2); break;
+    }
+
+    ASSERT_EQ(answer, result);
+}
+
+TEST_P(StructOps3TestSuite, DataDrivenTest)
+{
+    test(std::get<0>(GetParam()),
+         std::get<1>(GetParam()),
+         std::get<2>(GetParam()),
+         std::get<3>(GetParam()));
+}
+
+} // namespace
+
+INSTANTIATE_TEST_SUITE_P(
+    StructOps3TestGroup,
+    StructOps3TestSuite,
+    testing::Values(
+            // Basic operations
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::EQ, true),
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::NE, false),
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::LE, true),
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::GE, true),
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::LT, false),
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::GT, false),
+
+            std::make_tuple(MyStruct{1, 0, nullptr, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::NE, true),
+            std::make_tuple(MyStruct{1, 0, nullptr, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::GE, true),
+            std::make_tuple(MyStruct{1, 0, nullptr, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::GT, true),
+            std::make_tuple(MyStruct{1, 0, nullptr, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::LE, false),
+            std::make_tuple(MyStruct{1, 0, nullptr, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::LT, false),
+
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{1, 0, nullptr, nullptr}, ComparisonTest::NE, true),
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{1, 0, nullptr, nullptr}, ComparisonTest::LE, true),
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{1, 0, nullptr, nullptr}, ComparisonTest::LT, true),
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{1, 0, nullptr, nullptr}, ComparisonTest::GE, false),
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{1, 0, nullptr, nullptr}, ComparisonTest::GT, false),
+
+            // Deep comparison
+            std::make_tuple(MyStruct{0, 0, (char*)1, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::GT, true),
+            std::make_tuple(MyStruct{0, 0, (char*)1, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::EQ, false),
+            std::make_tuple(MyStruct{0, 0, (char*)1, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::NE, true),
+
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{0, 0, (char*)1, nullptr}, ComparisonTest::LT, true),
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{0, 0, (char*)1, nullptr}, ComparisonTest::EQ, false),
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{0, 0, (char*)1, nullptr}, ComparisonTest::NE, true),
+
+            // Float comparison
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{0, std::numeric_limits<float>::epsilon()/(float)1.1, nullptr, nullptr}, ComparisonTest::EQ, true),
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{0, std::numeric_limits<float>::epsilon()*2, nullptr, nullptr},   ComparisonTest::EQ, false),
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{0, std::numeric_limits<float>::epsilon()*2, nullptr, nullptr},   ComparisonTest::LT, true),
+
+            // Custom comparer
+            std::make_tuple(MyStruct{0, 0, nullptr, intPtr1}, MyStruct{0, 0, nullptr, intPtr2}, ComparisonTest::EQ, true),
+
+            // Stub
+            std::make_tuple(MyStruct{0, 0, nullptr, nullptr}, MyStruct{0, 0, nullptr, nullptr}, ComparisonTest::EQ, true)
+    ));
