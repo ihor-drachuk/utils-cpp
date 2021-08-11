d251f3b4a30ac5fadaf6865e24e6c50e288ce086
Ihor Drachuk <ihor.drachuk@gmail.com>
2021-08-11 18:50:44 +0300

    Updated ONCE macro

--- headers/utils-cpp/once.h
+++ headers/utils-cpp/once.h
@@ -3,9 +3,11 @@
 //! Provides ability to execute code only once.
 //! Be careful, it uses static flag that can be common for all instances of class.
 
-#define ONCE(flag, code) \
+#define ONCE_NAMED(flag, code) \
     static bool _flag##flag = true; \
     if (_flag##flag) { \
         _flag##flag = false; \
         code; \
     }
+
+#define ONCE(code) ONCE_NAMED(flag, code)
