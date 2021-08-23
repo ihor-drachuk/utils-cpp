e20da1ba8ce46aa513d27e5a66e5d358d6a1d637
Ihor Drachuk <ihor.drachuk@gmail.com>
2021-08-23 19:40:52 +0300

    More options in declareptr.h

--- headers/utils-cpp/declareptr.h
+++ headers/utils-cpp/declareptr.h
@@ -7,9 +7,18 @@
     using class_name##CPtr = std::shared_ptr<const class_name>; \
     using class_name##WPtr = std::weak_ptr<class_name>;
 
+#define DECLARE_PTR_NFD(class_name) \
+    using class_name##Ptr = std::shared_ptr<class_name>; \
+    using class_name##CPtr = std::shared_ptr<const class_name>; \
+    using class_name##WPtr = std::weak_ptr<class_name>;
 
 #define DECLARE_PTR_STRUCT(class_name) \
     struct class_name; \
+    using class_name##Ptr = std::shared_ptr<class_name>; \
+    using class_name##CPtr = std::shared_ptr<const class_name>; \
+    using class_name##WPtr = std::weak_ptr<class_name>;
+
+#define DECLARE_PTR_STRUCT_NFD(class_name) \
     using class_name##Ptr = std::shared_ptr<class_name>; \
     using class_name##CPtr = std::shared_ptr<const class_name>; \
     using class_name##WPtr = std::weak_ptr<class_name>;
