ec8e3f48af8ed5c56e31b1a20041d480cad3bc4c
Ihor Drachuk <ihor.drachuk@gmail.com>
2022-02-07 23:55:23 +0200

    Added tuple_utils

--- headers/utils-cpp/function_traits.h
+++ headers/utils-cpp/function_traits.h
@@ -5,18 +5,23 @@
 #include <type_traits>
 
 template<typename T>
-struct function_traits;
+struct function_traits_func;
 
 template<typename R, typename... Args>
-struct function_traits<std::function<R(Args...)>>
+struct function_traits_func<std::function<R(Args...)>>
 {
     static constexpr size_t args_count = sizeof...(Args);
 
-    using result_type = R;
+    using return_type = R;
+
+    using args = std::tuple<Args...>;
 
     template <size_t I>
     struct arg
     {
-        using type = typename std::tuple_element<I, std::tuple<Args...>>::type;
+        using type = typename std::tuple_element<I, args>::type;
     };
 };
+
+template<typename T>
+struct function_traits : public function_traits_func< decltype(std::function(std::declval<T>())) > {};
