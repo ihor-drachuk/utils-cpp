93897cbbcbf5e756902612b260f4505e8b3c9d90
Ihor Drachuk <ihor.drachuk@gmail.com>
2022-04-07 23:14:37 +0300

    Added overloaded.h

---
+++ headers/utils-cpp/overloaded.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace UtilsCpp {
+template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+} // namespace UtilsCpp
