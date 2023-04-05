27853f615730a8cca406ed8cb01e437991d6e414
Moritz 'Morty' Strübe <moritz.struebe@mathema.de>
2023-04-05 08:41:33 +0200

    ctor.h: Add helper macros for the rule of 5
    See https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rc-five

---
+++ headers/utils-cpp/ctor.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// These are helper-macros for the rule of 3/5
+// https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rc-five
+
+#define NO_COPY(classname)                \
+   classname(const classname &) = delete; \
+   classname &operator=(const classname &) = delete
+
+#define NO_MOVE(classname)           \
+   classname(classname &&) = delete; \
+   classname &operator=(classname &&) = delete
+
+#define NO_COPY_MOVE(classname) \
+   NO_COPY(classname);          \
+   NO_MOVE(classname)
+
+#define DEFAULT_COPY(classname)            \
+   classname(const classname &) = default; \
+   classname &operator=(const classname &) = default
+
+#define DEFAULT_MOVE(classname)       \
+   classname(classname &&) = default; \
+   classname &operator=(classname &&) = default
+
+#define DEFAULT_COPY_MOVE(classname) \
+   DEFAULT_COPY(classname);          \
+   DEFAULT_MOVE(classname)
