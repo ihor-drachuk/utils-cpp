51438b0eb1dd01ff052336b78532e003838dd712
Ihor Drachuk <ihor.drachuk@gmail.com>
2023-05-20 03:11:25 +0300

    container_utils: added -ref and -cref variants of find

--- headers/utils-cpp/container_utils.h
+++ headers/utils-cpp/container_utils.h
@@ -3,54 +3,212 @@
 #include <algorithm>
 #include <type_traits>
 #include <utility>
+#include <cassert>
+
+/*  Overview
+ *
+ *  There are functions, which find value in container. By default copy of value is stored internally.
+ *   - find        (container, value)
+ *   - find_if     (container, value, predicate)
+ *   - find_in_set (set-container, value)
+ *   - find_in_map (map-container, value)
+ *
+ *  There are additional modifications of find functions:
+ *    _cref  -  internally stores reference to found item instead of copying
+ *    _ref  -   same as above, but allows inplace modification
+ *
+ *  Returned object is similar to `std::optional`, but also has `index` method:
+ *   - operator  bool() const
+ *   - bool      has_value() const
+ *   - const T&  value() const
+ *   - T         value_or(const T& alternative) const
+ *   - size_t    index() const  -- unavailable for set and map containers!
+*/
 
 namespace utils_cpp {
 
-template<typename T>
-class SearchResult // Similar to std::optional<T>, but also has `index()` method
+namespace Internal {
+
+template<typename>   struct IsAnyRef                            : std::false_type {};
+template<typename T> struct IsAnyRef<std::reference_wrapper<T>> : std::true_type {};
+template<typename>   struct IsCRef                                  : std::false_type {};
+template<typename T> struct IsCRef<std::reference_wrapper<const T>> : std::true_type {};
+
+template<typename T> struct Unwrap                            { using type = T; };
+template<typename T> struct Unwrap<std::reference_wrapper<T>> { using type = std::remove_const_t<T>; };
+
+template<typename T, typename UT, bool rw_>
+class PartBase
+{
+public:
+    PartBase() = default;
+    PartBase(const T& data): m_data(data) {}
+
+    bool has_value() const { return m_data.has_value(); }
+    explicit operator bool() const { return has_value(); };
+
+    const T& value() const { return m_data.value(); }
+
+    template<bool rw = rw_, typename std::enable_if_t<rw>* = nullptr>
+    T& value() { return m_data.value(); }
+
+    UT value_or(const UT& altValue) const { return has_value() ? value() : altValue; }
+
+    operator std::optional<UT>() const { return has_value() ? std::optional<UT>(value()) : std::optional<UT>(); }
+
+private:
+    std::optional<T> m_data;
+};
+
+
+template<typename T, typename UT, bool rw_>
+class PartIndex : public PartBase<T, UT, rw_>
+{
+public:
+    PartIndex() = default;
+    PartIndex(const T& data, size_t index): PartBase<T, UT, rw_>(data), m_index(index) {}
+
+    size_t index() const { return static_cast<const PartBase<T, UT, rw_>*>(this)->value(), m_index; };
+
+private:
+    size_t m_index{};
+};
+
+template<typename T, typename UT, bool rw_, bool needIndex_,
+         typename Base = std::conditional_t<needIndex_,
+                                            PartIndex<T, UT, rw_>,
+                                            PartBase<T, UT, rw_>>>
+class PartOperators : public Base
+{
+public:
+    PartOperators() = default;
+
+    template<bool needIndex = needIndex_, typename std::enable_if_t<needIndex>* = nullptr>
+    PartOperators(const T& data, size_t index): Base(data, index) {}
+
+    template<bool needIndex = needIndex_, typename std::enable_if_t<!needIndex>* = nullptr>
+    PartOperators(const T& data): Base(data) {}
+
+    template<bool rw = rw_, typename std::enable_if_t<rw>* = nullptr> UT& operator* () { return Base::value(); };
+    template<bool rw = rw_, typename std::enable_if_t<rw>* = nullptr> UT* operator-> () { return &Base::value(); }
+    template<bool rw = rw_, typename std::enable_if_t<rw>* = nullptr> UT& value() { return Base::value(); }
+
+    const UT& operator* () const { return Base::value(); };
+    const UT* operator-> () const { return &Base::value(); }
+    const UT& value() const { return Base::value(); }
+};
+
+} // namespace Internal
+
+
+template<typename T,
+         bool needIndex_,
+         typename UT = typename Internal::Unwrap<T>::type,
+         bool rw = Internal::IsAnyRef<T>::value && !Internal::IsCRef<T>::value
+         >
+class SearchResult : public Internal::PartOperators<T, UT, rw, needIndex_>
 {
 public:
     SearchResult() = default;
-    SearchResult(const T& value, size_t index): m_result(std::make_pair(value, index)) {}
 
-    SearchResult(const SearchResult<T>&) = default;
-    SearchResult<T>& operator= (const SearchResult<T>&) = default;
+    template<bool needIndex = needIndex_, typename std::enable_if_t<needIndex>* = nullptr>
+    SearchResult(const T& data, size_t index): Internal::PartOperators<T, UT, rw, needIndex_>(data, index) {}
 
-    inline bool has_value() const { return m_result.has_value(); }
-    inline explicit operator bool() const { return has_value(); };
-
-    inline operator std::optional<T>() const { return has_value() ? std::optional<T>(value()) : std::optional<T>(); }
-
-    inline const T& operator* () const { return value(); };
-    inline const T* operator-> () const { return &value(); }
-    inline const T& value() const { return m_result.value().first; }
-
-    T value_or(const T& altValue) const { return has_value() ? value() : altValue; }
-
-    inline size_t index() const { return m_result.value().second; }
-
-private:
-    std::optional<std::pair<T, size_t>> m_result;
+    template<bool needIndex = needIndex_, typename std::enable_if_t<!needIndex>* = nullptr>
+    SearchResult(const T& data): Internal::PartOperators<T, UT, rw, needIndex_>(data) {}
 };
+
+// Regular find functions (copy stored)
 
 template<typename T = void,
          typename Container,
          typename RT = std::conditional_t<std::is_same_v<T, void>, typename Container::value_type, T>>
-SearchResult<RT> find(const Container& container, const RT& value)
+auto find(const Container& container, const RT& value)
 {
     auto it = std::find(std::cbegin(container), std::cend(container), value);
-    return (it == std::cend(container)) ? SearchResult<RT>() : SearchResult<RT>(*it, std::distance(std::cbegin(container), it));
+    return (it == std::cend(container)) ? SearchResult<RT, true>() :
+                                          SearchResult<RT, true>(*it, std::distance(std::cbegin(container), it));
 }
 
 template<typename T = void,
          typename Container,
          typename RT = std::conditional_t<std::is_same_v<T, void>, typename Container::value_type, T>,
          typename Callable>
-SearchResult<RT> find_if(const Container& container, const Callable& predicate)
+auto find_if(const Container& container, const Callable& predicate)
 {
     auto it = std::find_if(std::cbegin(container), std::cend(container), predicate);
-    return (it == std::cend(container)) ? SearchResult<RT>() : SearchResult<RT>(*it, std::distance(std::cbegin(container), it));
+    return (it == std::cend(container)) ? SearchResult<RT, true>() :
+                                          SearchResult<RT, true>(*it, std::distance(std::cbegin(container), it));
 }
+
+// Regular find functions (ref stored)
+
+template<typename T = void,
+         typename Container,
+         typename RT = std::conditional_t<std::is_same_v<T, void>, typename Container::value_type, T>>
+auto find_ref(Container& container, const RT& value)
+{
+    auto it = std::find(std::begin(container), std::end(container), value);
+    return (it == std::end(container)) ? SearchResult<std::reference_wrapper<RT>, true>() :
+                                         SearchResult<std::reference_wrapper<RT>, true>(*it, std::distance(std::begin(container), it));
+}
+
+template<typename T = void,
+         typename Container,
+         typename RT = std::conditional_t<std::is_same_v<T, void>, typename Container::value_type, T>,
+         typename Callable>
+auto find_if_ref(Container& container, const Callable& predicate)
+{
+    auto it = std::find_if(std::begin(container), std::end(container), predicate);
+    return (it == std::end(container)) ? SearchResult<std::reference_wrapper<RT>, true>() :
+                                         SearchResult<std::reference_wrapper<RT>, true>(*it, std::distance(std::begin(container), it));
+}
+
+// Regular find functions (const-ref stored)
+
+template<typename T = void,
+         typename Container,
+         typename RT = std::conditional_t<std::is_same_v<T, void>, typename Container::value_type, T>>
+auto find_cref(const Container& container, const RT& value)
+{
+    auto it = std::find(std::cbegin(container), std::cend(container), value);
+    return (it == std::cend(container)) ? SearchResult<std::reference_wrapper<const RT>, true>() :
+                                          SearchResult<std::reference_wrapper<const RT>, true>(*it, std::distance(std::cbegin(container), it));
+}
+
+template<typename T = void,
+         typename Container,
+         typename RT = std::conditional_t<std::is_same_v<T, void>, typename Container::value_type, T>,
+         typename Callable>
+auto find_if_cref(const Container& container, const Callable& predicate)
+{
+    auto it = std::find_if(std::cbegin(container), std::cend(container), predicate);
+    return (it == std::cend(container)) ? SearchResult<std::reference_wrapper<const RT>, true>() :
+                                          SearchResult<std::reference_wrapper<const RT>, true>(*it, std::distance(std::cbegin(container), it));
+}
+
+template<typename T = void,
+         typename Container,
+         typename RT = std::conditional_t<std::is_same_v<T, void>, typename Container::value_type, T>>
+auto find_ref(const Container& container, const RT& value)
+{
+    auto it = std::find(std::cbegin(container), std::cend(container), value);
+    return (it == std::cend(container)) ? SearchResult<std::reference_wrapper<const RT>, true>() :
+                                          SearchResult<std::reference_wrapper<const RT>, true>(*it, std::distance(std::cbegin(container), it));
+}
+
+template<typename T = void,
+         typename Container,
+         typename RT = std::conditional_t<std::is_same_v<T, void>, typename Container::value_type, T>,
+         typename Callable>
+auto find_if_ref(const Container& container, const Callable& predicate)
+{
+    auto it = std::find_if(std::cbegin(container), std::cend(container), predicate);
+    return (it == std::cend(container)) ? SearchResult<std::reference_wrapper<const RT>, true>() :
+                                          SearchResult<std::reference_wrapper<const RT>, true>(*it, std::distance(std::cbegin(container), it));
+}
+
+// Find in set
 
 template<typename T = void,
          typename Container,
@@ -58,8 +216,11 @@ template<typename T = void,
 std::optional<RT> find_in_set(const Container& container, const RT& value)
 {
     auto it = container.find(value);
-    return (it == std::cend(container)) ? std::optional<RT>() : std::optional<RT>(*it);
+    return (it == std::cend(container)) ? std::optional<RT>() :
+                                          std::optional<RT>(*it);
 }
+
+// Find in map
 
 template<typename T = void,
          typename Container,
@@ -68,7 +229,41 @@ template<typename T = void,
 std::optional<RT> find_in_map(const Container& container, const KT& value)
 {
     auto it = container.find(value);
-    return (it == std::cend(container)) ? std::optional<RT>() : std::optional<RT>(it->second);
+    return (it == std::cend(container)) ? std::optional<RT>() :
+                                          std::optional<RT>(it->second);
+}
+
+template<typename T = void,
+         typename Container,
+         typename RT = std::conditional_t<std::is_same_v<T, void>, typename Container::mapped_type, T>,
+         typename KT = typename Container::key_type>
+auto find_in_map_ref(Container& container, const KT& value)
+{
+    auto it = container.find(value);
+    return (it == std::end(container)) ? SearchResult<std::reference_wrapper<RT>, false>() :
+                                         SearchResult<std::reference_wrapper<RT>, false>(it->second);
+}
+
+template<typename T = void,
+         typename Container,
+         typename RT = std::conditional_t<std::is_same_v<T, void>, typename Container::mapped_type, T>,
+         typename KT = typename Container::key_type>
+auto find_in_map_ref(const Container& container, const KT& value)
+{
+    auto it = container.find(value);
+    return (it == std::cend(container)) ? SearchResult<std::reference_wrapper<const RT>, false>() :
+                                          SearchResult<std::reference_wrapper<const RT>, false>(it->second);
+}
+
+template<typename T = void,
+         typename Container,
+         typename RT = std::conditional_t<std::is_same_v<T, void>, typename Container::mapped_type, T>,
+         typename KT = typename Container::key_type>
+auto find_in_map_cref(const Container& container, const KT& value)
+{
+    auto it = container.find(value);
+    return (it == std::cend(container)) ? SearchResult<std::reference_wrapper<const RT>, false>() :
+                                          SearchResult<std::reference_wrapper<const RT>, false>(it->second);
 }
 
 } // namespace utils_cpp
