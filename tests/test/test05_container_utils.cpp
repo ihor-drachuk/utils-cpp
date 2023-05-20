97dc8ed88c3273e3273f729702cbab0d376d0650
Ihor Drachuk <ihor.drachuk@gmail.com>
2023-05-20 03:11:25 +0300

    container_utils: added -ref and -cref variants of find

--- tests/test/test05_container_utils.cpp
+++ tests/test/test05_container_utils.cpp
@@ -7,9 +7,13 @@
 #include <list>
 #include <utils-cpp/container_utils.h>
 #include <utils-cpp/comparison_traits.h>
+#include <utils-cpp/as_const.h>
+#include <utils-cpp/checkmethod.h>
+
+CREATE_CHECK_METHOD(index)
 
 
-TEST(utils_cpp, ContainerUtilsTest)
+TEST(utils_cpp, ContainerUtilsTest_ByCopy)
 {
     std::vector<int> vector {1, 2, 3};
     std::list<int> list {1, 2, 3};
@@ -35,6 +39,8 @@ TEST(utils_cpp, ContainerUtilsTest)
     ASSERT_FALSE((std::is_assignable<bool, decltype(utils_cpp::find(vector, 2))>::value));
     ASSERT_FALSE((std::is_assignable<int, decltype(utils_cpp::find(vector, 2))>::value));
     ASSERT_TRUE((std::is_assignable<std::optional<int>, decltype(utils_cpp::find(vector, 2))>::value));
+
+    ASSERT_FALSE((std::is_assignable_v<decltype(utils_cpp::find(vector, 2)), int>));
 
     if (utils_cpp::find(vector, 1)) ASSERT_TRUE(true);
     if (!utils_cpp::find(vector, 4)) ASSERT_TRUE(true);
@@ -73,4 +79,105 @@ TEST(utils_cpp, ContainerUtilsTest)
     ASSERT_FALSE(utils_cpp::find_in_map(map, 4).has_value());
     ASSERT_TRUE(utils_cpp::find_in_map(unordered_map, 1).has_value());
     ASSERT_FALSE(utils_cpp::find_in_map(unordered_map, 4).has_value());
+}
+
+TEST(utils_cpp, ContainerUtilsTest_value_or)
+{
+    ASSERT_EQ(utils_cpp::find       (std::vector<int>   {1, 2, 3, 4}, 1)                        .value_or(10), 1);
+    ASSERT_EQ(utils_cpp::find       (std::list<int>     {1, 2, 3, 4}, 2)                        .value_or(10), 2);
+    ASSERT_EQ(utils_cpp::find_in_set(std::set<int>      {1, 2, 3, 4}, 3)                        .value_or(10), 3);
+    ASSERT_EQ(utils_cpp::find_in_map(std::map<int, int> {{1, 11}, {2, 22}, {3, 33}, {4, 44}}, 4).value_or(10), 44);
+    ASSERT_EQ(utils_cpp::find       (std::vector<int>   {1, 2, 3, 4}, 5)                        .value_or(10), 10);
+}
+
+TEST(utils_cpp, ContainerUtilsTest_ByRef)
+{
+    std::vector<int> container {1, 2, 3, 4};
+    auto value = utils_cpp::find_ref(container, 2);
+    container[1] = 5;
+    ASSERT_EQ(*value, 5);
+
+    ASSERT_TRUE((std::is_assignable_v<decltype(*value), int>));
+    ASSERT_TRUE((CheckMethod::index_v<decltype(value)>));
+
+    *value = 2;
+    ASSERT_EQ(container[1], 2);
+}
+
+TEST(utils_cpp, ContainerUtilsTest_ByCRef)
+{
+    std::vector<int> container {1, 2, 3, 4};
+    auto valueA = utils_cpp::find_cref(container, 2);
+    container[1] = 5;
+    ASSERT_EQ(*valueA, 5);
+    ASSERT_EQ(valueA.index(), 1);
+    ASSERT_FALSE((std::is_assignable_v<decltype(*valueA), int>));
+    ASSERT_TRUE((CheckMethod::index_v<decltype(valueA)>));
+
+    auto valueB = utils_cpp::find_ref(utils_cpp::as_const(container), 3);
+    container[2] = 10;
+    ASSERT_EQ(*valueB, 10);
+    ASSERT_EQ(valueB.index(), 2);
+    ASSERT_FALSE((std::is_assignable_v<decltype(*valueB), int>));
+    ASSERT_TRUE((CheckMethod::index_v<decltype(valueB)>));
+}
+
+TEST(utils_cpp, ContainerUtilsTest_MapByRef)
+{
+    std::map<int, int> mapContainer {
+        {1, 11},
+        {2, 22},
+        {3, 33},
+        {4, 44}
+    };
+
+    auto value = utils_cpp::find_in_map_ref(mapContainer, 2);
+    ASSERT_EQ(*value, 22);
+    mapContainer[2] = 220;
+    ASSERT_EQ(*value, 220);
+    ASSERT_TRUE((std::is_assignable_v<decltype(*value), int>));
+    ASSERT_FALSE((CheckMethod::index_v<decltype(value)>));
+    value.value() = 2200;
+    ASSERT_EQ(mapContainer[2], 2200);
+}
+
+TEST(utils_cpp, ContainerUtilsTest_MapByCRef)
+{
+    {
+        std::map<int, int> mapContainer {
+            {1, 11},
+            {2, 22},
+            {3, 33},
+            {4, 44}
+        };
+
+        auto value = utils_cpp::find_in_map_ref(utils_cpp::as_const(mapContainer), 2);
+        ASSERT_EQ(*value, 22);
+        mapContainer[2] = 220;
+        ASSERT_EQ(*value, 220);
+        ASSERT_FALSE((std::is_assignable_v<decltype(*value), int>));
+        ASSERT_FALSE((CheckMethod::index_v<decltype(value)>));
+    }
+
+    {
+        std::map<int, int> mapContainer {
+            {1, 11},
+            {2, 22},
+            {3, 33},
+            {4, 44}
+        };
+
+        auto value = utils_cpp::find_in_map_cref(mapContainer, 2);
+        ASSERT_EQ(*value, 22);
+        mapContainer[2] = 220;
+        ASSERT_EQ(*value, 220);
+        ASSERT_FALSE((std::is_assignable_v<decltype(*value), int>));
+        ASSERT_FALSE((CheckMethod::index_v<decltype(value)>));
+    }
+}
+
+TEST(utils_cpp, ContainerUtilsTest_set)
+{
+    std::set<int> container {1, 2, 3, 4};
+    //auto r = utils_cpp::find_ref(container, 2); -- should fail, because std::set::iterator isn't modifiable
 }
