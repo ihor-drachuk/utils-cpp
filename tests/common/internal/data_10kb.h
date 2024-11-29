c18273851bcb244cef361909a77c3b0880992d58
Ihor Drachuk <ihor-drachuk-libs@pm.me>
2024-11-29 02:32:12 +0200

    Added xor.h

---
+++ tests/common/internal/data_10kb.h
@@ -0,0 +1,9 @@
+/* License:  MIT
+ * Source:   https://github.com/ihor-drachuk/utils-qt
+ * Contact:  ihor-drachuk-libs@pm.me  */
+
+#include <array>
+#include <cstdint>
+
+const std::array<uint8_t, 10240>& data_10kb_1();
+const std::array<uint8_t, 10240>& data_10kb_2();
