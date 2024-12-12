4a160a4708663a175796b65525d872cca1f781b0
Ihor Drachuk <ihor-drachuk-libs@pm.me>
2024-12-12 23:24:57 +0200

    macros: void return support

--- headers/utils-cpp/macros.h
+++ headers/utils-cpp/macros.h
@@ -5,10 +5,18 @@
 #define STRINGIFY(x) #x
 #define TO_STRING(x) STRINGIFY(x)
 
-#define COND_ACT_RETURN(Condition, Action, ReturnValue) \
+#define COND_ACT_RETURN_VAL(Condition, Action, ReturnValue) \
     do { \
         if (Condition) { \
             Action; \
             return ReturnValue; \
         } \
     } while (0)
+
+#define COND_ACT_RETURN(Condition, Action) \
+do { \
+        if (Condition) { \
+            Action; \
+            return; \
+    } \
+} while (0)
